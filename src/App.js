8d7b9f7b9c219878af5626e7fd64fbda305ca08d
RafiurRahmanSaikat <md.rafiur.rahman.saikat@gmail.com>
2023-03-10 19:53:29 +0600

    basic Setup Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { Navbar, SideMenu } from "./components";
+
+const Container = styled.div``;
+const Main = styled.div``;
+const Wrapper = styled.div``;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <SideMenu />
+      <Main>
+        <Navbar />
+      </Main>
+    </Container>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      nav BAR
+    </div>
+  );
+};
+
+export default Navbar;
